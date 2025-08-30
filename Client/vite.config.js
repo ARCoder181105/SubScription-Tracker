8bf04a899835f403018524bfa65d4ddac4c3a7ee
Aditya Rana <adityarana181105@gmail.com>
2025-08-30 20:05:02 +0530

    done

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:6969',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL, // ✅ loads from .env
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+    },
   }
 })
